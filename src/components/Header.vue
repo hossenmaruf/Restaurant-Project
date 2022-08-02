bef0ed818c450e9fa554d093c17441575d46ac70
hossenmaruf <severusmaruf740@gmail.com>
2022-08-03 03:33:25 +0600

    nav-bar

---
+++ src/components/Header.vue
@@ -0,0 +1,47 @@
+<template>
+
+   <div  class = nav >  
+
+    <a  href="" > Home </a>
+   <a  href="" > Add Restaurant </a>
+   <a  href="" > Update Restaurant </a>
+   <a  href="" > LogOut </a>
+    
+   </div>
+  
+  
+
+</template>
+
+<script>
+ 
+ export default {
+    name: "HeaderView" ,
+ } ;
+
+</script>
+
+<style>
+
+  .nav {
+    background-color: rgb(18, 222, 42);
+    overflow: hidden;
+  }
+
+  .nav a {
+    float: left;
+    color: rgb(71, 12, 234);
+    padding: 14px 16px;
+    text-align: center;
+    font-size: 17px;
+    text-decoration: none;
+    margin-right: 5px;
+  }
+
+  .nav a:hover {
+    background-color: rgb(199, 233, 29);
+    color: rgb(0, 34, 255);
+  }
+ 
+   
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,16 @@
 <template>
+  <Header />
+
   <h1>hello, this is home page</h1>
 </template>
 
 <script>
+import Header from "./Header.vue";
+
 export default {
   name: "HomePage",
+  components: { Header },
+
   mounted() {
     let user = localStorage.getItem("user-info");
     if (!user) {
